78d2bca37c416d0a56608232c78fb47bf0b951a9
Bianca-Pinto <71729898+Bianca-Pinto@users.noreply.github.com>
2021-11-28 00:52:23 -0300

    Update tempo_execucao.h

--- tempo_execucao.h
+++ tempo_execucao.h
@@ -7,10 +7,10 @@
 #include "algoritmos_ordenacao.h"
 
 void t_bubblesort (int v[], int n);
-void t_quicksort(int v[], int n);
+void t_quicksort(int v[], int inicio, int fim);
 void t_insertion_sort (int v[], int n);
 void t_selection_sort(int v[], int n);
 void t_mergesort(int v[], int n);
-//void t_heapsort(int v[], int n);
+void t_heapsort(int v[], int n);
 
 #endif //ALGORITMOS_ORDENACAO_TEMPO_EXECUCAO_H
